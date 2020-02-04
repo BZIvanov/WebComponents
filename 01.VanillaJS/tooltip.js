25e8323ca41b8515de66833e7f3010b931eccaca
BZIvanov <ivanov.biser@gmx.com>
2020-02-04 18:31:09 +0200

    initial commit

---
+++ 01.VanillaJS/tooltip.js
@@ -0,0 +1,38 @@
+class Tooltip extends HTMLElement {
+  constructor() {
+    super();
+    this._tooltipContainer;
+    this._tooltipText = 'Some dummy tooltip text.';
+  }
+
+  // this callback is called, when our custom component is already attached to the DOM, so here is the right place to do something with it
+  connectedCallback() {
+    if (this.hasAttribute('text')) {
+      this._tooltipText = this.getAttribute('text');
+    }
+    const tooltipIcon = document.createElement('span');
+    tooltipIcon.textContent = ' (?)';
+    tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
+    tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
+    this.appendChild(tooltipIcon);
+    this.style.position = 'relative';
+  }
+
+  _showTooltip() {
+    this._tooltipContainer = document.createElement('div');
+    this._tooltipContainer.textContent = this._tooltipText;
+    this._tooltipContainer.style.backgroundColor = 'black';
+    this._tooltipContainer.style.color = 'white';
+    this._tooltipContainer.style.position = 'absolute';
+    this._tooltipContainer.style.zIndex = '10';
+    this.appendChild(this._tooltipContainer);
+  }
+
+  _hideTooltip() {
+    this.removeChild(this._tooltipContainer);
+  }
+}
+
+// customElements is built-in javascript object with define method which allows us to create our custom tags
+// the name of the tag must contains a dash("-")
+customElements.define('uc-tooltip', Tooltip);
