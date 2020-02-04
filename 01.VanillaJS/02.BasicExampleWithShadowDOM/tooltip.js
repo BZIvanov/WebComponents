37c145be75ba0487ead6a0a329de7d7c75a92770
BZIvanov <ivanov.biser@gmx.com>
2020-02-04 18:43:50 +0200

    shadow DOM vanilla js

---
+++ 01.VanillaJS/02.BasicExampleWithShadowDOM/tooltip.js
@@ -0,0 +1,47 @@
+class Tooltip extends HTMLElement {
+  constructor() {
+    super();
+    this._tooltipContainer;
+    this._tooltipText = 'Some dummy tooltip text.';
+    this.attachShadow({ mode: 'open' });      // here we make our custom components using the shadow DOM
+    // we have access to shadowRoot function because we allowed shadowDOM and these styles will be only applied for our custom component
+    // because our custom component is now in the shadow dom we dont get the content from the normal dom so we have to use slot element which is similar to props.children from React
+    // if in the normal DOM we dont have content between the tags for our costum component, whatever is in the slot will be used as default content 
+    this.shadowRoot.innerHTML = `
+        <style>
+            div {
+                background-color: black;
+                color: white;
+                position: absolute;
+                z-index: 10;
+            }
+        </style>
+        <slot>Some default</slot>
+        <span> (?)</span>
+    `;
+  }
+
+  connectedCallback() {
+    if (this.hasAttribute('text')) {
+      this._tooltipText = this.getAttribute('text');
+    }
+    const tooltipIcon = this.shadowRoot.querySelector('span');
+    tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
+    tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
+    // now we append out elements to the shadow DOM with the provided shadowRoot function
+    this.shadowRoot.appendChild(tooltipIcon);
+    this.style.position = 'relative';
+  }
+
+  _showTooltip() {
+    this._tooltipContainer = document.createElement('div');
+    this._tooltipContainer.textContent = this._tooltipText;
+    this.shadowRoot.appendChild(this._tooltipContainer);
+  }
+
+  _hideTooltip() {
+    this.shadowRoot.removeChild(this._tooltipContainer);
+  }
+}
+
+customElements.define('uc-tooltip', Tooltip);
