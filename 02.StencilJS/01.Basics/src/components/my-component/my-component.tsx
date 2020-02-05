9355977e8f39d675aa6be73dabaed24a7391dc8b
BZIvanov <ivanov.biser@gmx.com>
2020-02-05 18:04:57 +0200

    stencil info added

---
+++ 02.StencilJS/01.Basics/src/components/my-component/my-component.tsx
@@ -0,0 +1,39 @@
+import { Component, Prop, h } from '@stencil/core';
+import { format } from '../../utils/utils';
+
+@Component({
+  tag: 'my-component',             // this is how our custom tag will be called and it must have at least 1 dash
+  styleUrl: 'my-component.css',
+  shadow: true
+})
+export class MyComponent {
+  /**
+   * The first name
+   */
+  @Prop() first: string;    // Prop decorator will watch for changes on the variable and call render method if changes were made
+
+  /**
+   * The middle name
+   */
+  @Prop({
+    reflectToAttr: true,
+  }) middle: string;         // reflectToAttr will actually change the attribute value, not just update the DOM
+
+  /**
+   * The last name
+   */
+  @Prop({
+    mutable: true,
+  }) last: string;          // mutable will allow us to change the value from inside the class, not only from outside
+
+  private getText(): string {
+    return format(this.first, this.middle, this.last);
+  }
+
+  render() {
+    return (
+      <div>Hello, World! I'm {this.getText()}
+        <slot />
+      </div>);
+  }
+}
--- README.md
+++ README.md
@@ -1,5 +1,15 @@
-# Additional info
+## Vanilla JS
+
+### Additional info
 
 Content between tags for custom element is not part of the shadow DOM, it is still in the light DOM. So if we want to style that content we can do it in the html or external css file, but for custom component we usually dont want that.
 
 So we can use special selector in our custom component which is **::slotted(.my-item)** but keep in mind selectors we use in the round brackets will only be applied to the first level children elements, not for the nested.
+
+## Stencil JS
+
+**IMPORTANT** when placing content between the tags for custom element and getting that content in a slot tag for example we have access only the top level elements.
+
+Also the content between custom tags is still in the light DOM.
+
+Prop's are immutable, meaning we cant change them from inside the class, only changeable as incoming from outside. But thet can be changed if we provide configuration to Prop with mutable: true.
